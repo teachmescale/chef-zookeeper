1a1f0380c5158a534dd6a75f6e0a2848d99ae32f
Max Korenkov <mkorenkov@gmail.com>
2014-02-14 08:51:32 +0100

    syntax

--- attributes/default.rb
+++ attributes/default.rb
@@ -18,7 +18,7 @@ default[:zookeeper][:autorun_ebs_volume_recipe] = false
 default[:zookeeper][:ebs_vol_dev] = "/dev/sdp"
 default[:zookeeper][:ebs_vol_size] = 10
 
-default[:zookeeper][:search_cluster_servers] = not Chef::Config.solo or node.recipes.include?("chef-solo-search")
+default[:zookeeper][:search_cluster_servers] = (not Chef::Config.solo or node.recipes.include?("chef-solo-search"))
 default[:zookeeper][:auto_add_current_node] = node.role?("zookeeper")
 # ZK Cluster defaults
 default[:zookeeper][:cluster_servers] = [{
